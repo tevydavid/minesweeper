66c7978ee81dc267aae9d85894d55d30d2422360
App Academy Student <student@appacademy.io>
2015-10-30 12:03:20 -0700

    almost done with Tile class

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,32 +1,63 @@
 class Tile
-  def initialize(board)
+
+  attr_reader :pos, :bombed, :flagged, :revealed, :neighbor_bomb_count
+
+  def initialize(board, pos, bombed = false)
     @board = board
-    @value =
+    @pos = pos
+    @bombed = bombed
+    @flagged = false
+    @revealed = false
   end
 
   def reveal!
+    @revealed = true
   end
 
+  def flag!
+    @flagged = true
+  end
 
 
   def inspect
+    "bombed? :#{@bombed}, flagged: #{@flagged}, revealed: #{@revealed}, neighbor_bomb_count: #{@neighbor_bomb_count}"
   end
 
   def neighbors
+    neighbor_positions = []
+    (-1..1).each do |row|
+      (-1..1).each do |col|
+        next if row ==0 && col == 0
+         possible_position = [@pos[0] + row, @pos[1] + col]
+         neighbor_positions << possible_position if valid?(possible_position)
   end
 
+  def valid?(position)
+    position[0].between?(0, 8) && position[1].between?(0, 8)
+  end
+
+
+
   def neighbor_bomb_count
+    @neighbor_bomb_count
   end
 
 
 end
 
 class Board
+  attr_reader :board
+
+  def initialize
+
+
+        #call each tile.neighbors
+  end
 
 end
 
 class Game
-  def initialize
+  def initiali
 
   end
 
